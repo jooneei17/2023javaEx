e196ae21674ecea691d9b6b83f430dd0e00c8d32
jooneei17 <82086303+jooneei17@users.noreply.github.com>
2023-01-16 12:53:44 +0900

    Update CountMoney.java

--- 20230113/CountMoney.java
+++ 20230113/CountMoney.java
@@ -6,7 +6,7 @@ public class CountMoney {
 	public static void main(String[] args) {
 		int[] moneyUnit = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 		int amount = 237980;
-		int[] moneyCount = new int[8];
+		int[] moneyCount = new int[moneyUnit.length];//가급적이면 변수 형태로 쓰기
 		
 		for (int i = 0; i < moneyUnit.length; i++) {
 			moneyCount[i] = amount / moneyUnit[i];
