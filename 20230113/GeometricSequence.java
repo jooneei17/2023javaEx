78a880a3f9f0f15b82438d55075f957202ccf788
jooneei17 <82086303+jooneei17@users.noreply.github.com>
2023-01-16 12:12:04 +0900

    Update GeometricSequence.java

--- 20230113/GeometricSequence.java
+++ 20230113/GeometricSequence.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class GeometricSequence {
 	public static void main(String[] args) {
+		
+		//다음 식처럼 계산하면 첫항에 대한 규칙을 구할 수 있다.
+		for (int i = 0; i < 20; i++) {
+			int val = 1;
+			for (int j = 0; j < i; j++) {
+				val *= 2;
+			}
+			System.out.print(val + " ");
+		}
+		
+		
 		//등비수열 ex) 3, 6, 12, 24, ...
 //		int num = 3;
 //		
